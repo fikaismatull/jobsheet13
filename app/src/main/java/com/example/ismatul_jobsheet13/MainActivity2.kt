ea2ed4ce6cfea6751ccffbdad254d152649a1406
NorHidayah <salma@gmail.com>
2022-09-28 09:11:50 +0700

    first commit

---
+++ app/src/main/java/com/example/ismatul_jobsheet13/MainActivity2.kt
@@ -0,0 +1,27 @@
+package com.example.ismatul_jobsheet13
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var namaView: TextView
+    private lateinit var kelasView: TextView
+    private lateinit var nisView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        namaView = findViewById(R.id.namaView)
+        kelasView = findViewById(R.id.kelasView)
+        nisView = findViewById(R.id.nisView)
+
+        val extras = intent.extras
+
+        namaView.text = extras?.getString("nama")
+        kelasView.text = extras?.getString("kelas")
+        nisView.text = extras?.getString("nis")
+    }
+}
+
